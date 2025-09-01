399f145c52c20dab4de3c9711d099179ae465bef
Muhammad Rhizal Rhomadon <155636063+rhizal24@users.noreply.github.com>
2025-09-01 15:15:10 +0700

    feat(modul2): menambahkan code input dan output

--- console/Program.cs
+++ console/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan sebuah angka");
+            int angka = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anda memasukkan angka: " + angka.ToString());
+            Console.ReadLine();
+        }
+    }
+}
